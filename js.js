037f32f757d919e0b4336d23cea5eb0fcbf7e2e2
Taha Ikbarya <100837774+thaha1233@users.noreply.github.com>
2023-01-07 14:02:57 +0200

    Add files via upload

---
+++ js.js
@@ -0,0 +1,38 @@
+// var well = document.getElementById("#well");
+
+// setTimeout(function () {
+    
+//     well.style.cssText=`
+//     display: block;
+//     `
+// 3000});
+
+
+// let scr = document.getElementById("#scr");
+
+// window.onscroll = function() {
+// if(window.screenY >= 50){
+    
+//     scr.style.display="block";
+//     console.log("huhujh");
+
+// }
+
+// else{
+//     scr.style.display="none";
+
+// }
+
+
+// };
+
+
+
+
+
+
+
+
+
+
+
